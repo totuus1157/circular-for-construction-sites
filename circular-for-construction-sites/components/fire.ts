dbb56ca7fa649c5247e66135d86051bd2dc3b4c5
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-09-23 13:11:36 +0900

    Firebaseの初期化処理を追加

---
+++ circular-for-construction-sites/components/fire.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACI7uHPvhwTE-ajRqZnnM35aGBDP2o3QE",
+  authDomain: "circular-for-construction-site.firebaseapp.com",
+  projectId: "circular-for-construction-site",
+  storageBucket: "circular-for-construction-site.appspot.com",
+  messagingSenderId: "427904359731",
+  appId: "1:427904359731:web:9a5fd6b248ace8f91f7246"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
