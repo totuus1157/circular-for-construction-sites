85592e58d2633dfd24c1910532ab2035b497daf2
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-09-26 14:32:07 +0900

    トップページ作成

--- circular-for-construction-sites/pages/index.tsx
+++ circular-for-construction-sites/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const Home: NextPage = (): JSX.Element => {
   return (
     <div className={styles.container}>
-          <Header title="Top Page" />
+      <Header title="Top Page" />
 
       <main className={styles.main}>
         <h1 className={styles.title}>電子観覧版</h1>
@@ -13,13 +14,17 @@ const Home: NextPage = () => {
         <p className={styles.description}>for 建設現場</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>ログイン</h2>
-          </a>
+          <Link href="/login">
+            <a className={styles.card}>
+              <h2>ログイン</h2>
+            </a>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>新規登録</h2>
-          </a>
+          <Link href="/register">
+            <a className={styles.card}>
+              <h2>新規登録</h2>
+            </a>
+          </Link>
         </div>
       </main>
     </div>
