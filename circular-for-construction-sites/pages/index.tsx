d68a0c737a390713be1a521061441db7694e2237
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-10-01 17:16:00 +0900

    トップページにアクセスした際のログアウト処理を追加

--- circular-for-construction-sites/pages/index.tsx
+++ circular-for-construction-sites/pages/index.tsx
@@ -2,6 +2,11 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
+import { getAuth, signOut } from "firebase/auth";
+import "../components/fire";
+
+const auth = getAuth();
+signOut(auth);
 
 const Home: NextPage = (): JSX.Element => {
   return (
