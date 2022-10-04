6da318e4aa50f34f3f3ae58b5580bcef697d2f0c
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-10-04 13:30:50 +0900

    next-remove-imports 設定追加

--- circular-for-construction-sites/next.config.js
+++ circular-for-construction-sites/next.config.js
@@ -1,7 +1,19 @@
+// next.config.js
+const removeImports = require("next-remove-imports")({
+  test: /node_modules([\s\S]*?)\.(tsx|ts|js|mjs|jsx)$/,
+  matchImports: "\\.(less|css|scss|sass|styl)$",
+});
+
+module.exports = removeImports({
+  webpack(config, options) {
+    return config;
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
