ab2545a04e76e44a984116e9c78d393c5cf7a9d0
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-10-04 17:32:18 +0900

    News 表示機能作成

--- circular-for-construction-sites/components/Articles.tsx
+++ circular-for-construction-sites/components/Articles.tsx
@@ -1,38 +1,49 @@
 import { useState, useEffect, SetStateAction } from "react";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { db } from "../components/firebase";
+import { collectionGroup, getDocs } from "firebase/firestore";
 import Stack from "react-bootstrap/Stack";
 import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../components/fire";
 
-const db = getFirestore();
-
-function Articles(props) {
-  const { user } = props;
+function Articles() {
   const mydata: SetStateAction<any[]> = [];
   const [data, setData] = useState(mydata);
 
-  console.log("data: ", data);
-  console.log("user: ", user);
+  const areaName = (area: string): string => {
+    return area.toUpperCase();
+  };
+  const sectionName = (section: string): string => {
+    return section.slice(0, 1).toUpperCase() + section.slice(1);
+  };
+  const postDate = (timestamp: { second: any; toDate: () => any }): string => {
+    const date = timestamp.toDate();
+    return `${date.getMonth() + 1}月${date.getDate()}日`;
+  };
 
   useEffect((): void => {
-    getDocs(collection(db, "users", user?.email, "Articles")).then(
-      (snapshot): void => {
-        snapshot.forEach((document): void => {
-          const doc = document.data();
-          mydata.push(
-            <Card>
-              <Card.Body>
-                <Card.Text key={document.id}>{doc.article}</Card.Text>
-                <Card.Footer className="text-muted">Footer</Card.Footer>
-              </Card.Body>
-            </Card>
-          );
-          console.log("mydata: ", mydata);
-        });
-        setData(mydata);
-      }
-    );
+    getDocs(collectionGroup(db, "articles")).then((snapshot): void => {
+      snapshot.forEach((document): void => {
+        const doc = document.data();
+        mydata.push(
+          <Card border="dark">
+            <Card.Body>
+              <Card.Header className="text-muted">
+                From: {doc.name}（エリア{areaName(doc.from.area)}
+                {"　"}
+                {sectionName(doc.from.section)}）{postDate(doc.timestamp)}
+              </Card.Header>
+              <Card.Text key={document.id}>{doc.article}</Card.Text>
+              <Card.Footer className="text-muted">
+                To: エリア{areaName(doc.to.area)}
+                {"　"}
+                {sectionName(doc.to.section)}
+              </Card.Footer>
+            </Card.Body>
+          </Card>
+        );
+      });
+      setData(mydata);
+    });
   }, []);
 
   return <Stack gap={1}>{data}</Stack>;
