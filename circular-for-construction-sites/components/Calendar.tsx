75ea9c2170aa9af6df1ea07508236ce4cadcee3f
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-10-05 14:42:42 +0900

    スケジュール読み込み機能を追加

--- circular-for-construction-sites/components/Calendar.tsx
+++ circular-for-construction-sites/components/Calendar.tsx
@@ -1,9 +1,26 @@
+import { useState, useEffect, SetStateAction } from "react";
+import { db } from "../components/firebase";
+import { collectionGroup, getDocs } from "firebase/firestore";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import jaLocale from "@fullcalendar/core/locales/ja";
 
 function Calendar(props): JSX.Element {
+  const mydata: SetStateAction<any[]> = [];
+  const [data, setData] = useState(mydata);
+
+  useEffect((): void => {
+    const docRef = collectionGroup(db, "schedules");
+    getDocs(docRef).then((snapshot): void => {
+      snapshot.forEach((document): void => {
+        const doc = document.data();
+        mydata.push({ title: doc.plan, start: doc.start, end: doc.end });
+      });
+      setData(mydata);
+    });
+  }, []);
+
   return (
     <FullCalendar
       plugins={[dayGridPlugin, timeGridPlugin]}
@@ -13,12 +30,12 @@ function Calendar(props): JSX.Element {
         right: "dayGridMonth,timeGridWeek,timeGridDay",
       }}
       initialView="dayGridMonth" // 初期表示のモードを設定する
-      events={"https://fullcalendar.io/api/demo-feeds/events.json"}
       allDaySlot={false}
       slotDuration="01:00:00"
       aspectRatio={0.75}
       locales={[jaLocale]}
       locale="ja"
+      events={data}
     />
   );
 }
