68cacf8299bfffe14998bdd28d7bcc2ade7fed64
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-10-06 17:46:43 +0900

    ログアウト後にトップページに戻る

--- circular-for-construction-sites/components/UserDisplay.tsx
+++ circular-for-construction-sites/components/UserDisplay.tsx
@@ -1,10 +1,16 @@
 import { auth } from "../components/firebase";
 import { signOut } from "firebase/auth";
+import { useRouter } from "next/router";
 
 function UserDisplay(props: { name: string | null }): JSX.Element {
   const { name } = props;
+  const router = useRouter();
+
   const logout = (): void => {
-    confirm("ログアウトしますか？") && signOut(auth);
+    confirm("ログアウトしますか？") &&
+      signOut(auth).then((): void => {
+        router.push("/");
+      });
   };
 
   return (
