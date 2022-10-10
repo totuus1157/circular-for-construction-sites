f1bd41e644638b520ba8709e1be08432d7541c65
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-10-10 15:10:46 +0900

    アーカイブ用ページ作成

---
+++ circular-for-construction-sites/components/Archives.tsx
@@ -0,0 +1,134 @@
+import { useState, useEffect, SetStateAction } from "react";
+import ArchiveButton from "../components/ArchiveButton";
+import ConfirmBadge from "../components/ConfirmBadge";
+import ArticleModal from "../components/ArticleModal";
+import { db } from "../components/firebase";
+import { collectionGroup, getDocs, query, orderBy } from "firebase/firestore";
+import Stack from "react-bootstrap/Stack";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+type Props = {
+  userInfoEmail: string;
+  userInfoName: string;
+  canAdmin: boolean;
+  counter: number;
+  setCounter: (arg0: number) => void;
+};
+
+function Archives(props: Props): JSX.Element {
+  const { userInfoEmail, userInfoName, canAdmin, counter, setCounter } = props;
+
+  type ContentData = { docId: string; title: string; article: string };
+
+  const contentArray: ContentData[] = [];
+  const cardArray: JSX.Element[] = [];
+  const [contentData, setContentData] = useState(contentArray);
+  const [cardData, setCardData] = useState(cardArray);
+  const [selectedID, setSelectedID] = useState("");
+  const [show, setShow] = useState(false);
+
+  useEffect((): void => {
+    setCounter(counter + 1);
+  }, [userInfoEmail, userInfoName]);
+
+  const areaName = (area: string): string => {
+    return area.toUpperCase();
+  };
+  const sectionName = (section: string): string => {
+    return section.slice(0, 1).toUpperCase() + section.slice(1);
+  };
+  const postDate = (timestamp: { second: any; toDate: () => any }): string => {
+    const date = timestamp.toDate();
+    return `${date.getMonth() + 1}月${date.getDate()}日`;
+  };
+
+  const targetArticle = contentData.find((article): boolean => {
+    return article.docId === selectedID;
+  });
+
+  const openModal = (docId: SetStateAction<string>): void => {
+    setSelectedID(docId);
+    setShow(true);
+  };
+
+  useEffect((): void => {
+    const docRef = collectionGroup(db, "articles");
+    const q = query(docRef, orderBy("timestamp", "desc"));
+    getDocs(q).then((snapshot): void => {
+      snapshot.forEach((document): void => {
+        const doc = document.data();
+        if (doc.archive === false) {
+          contentArray.push({
+            docId: document.id,
+            title: doc.title,
+            article: doc.article,
+          });
+          cardArray.push(
+            <Card key={document.id} border="dark">
+              <Card.Body>
+                <Card.Text className="small text-muted d-flex justify-content-between">
+                  From: {doc.name}（エリア{areaName(doc.from.area)}
+                  {"　"}
+                  {sectionName(doc.from.section)}）{postDate(doc.timestamp)}
+                </Card.Text>
+                <Card.Text className="d-flex justify-content-between">
+                  <Button
+                    variant="link"
+                    onClick={(): void => openModal(document.id)}
+                  >
+                    <strong>{doc.title}</strong>
+                  </Button>
+                  {(doc.email === userInfoEmail || canAdmin === true) && (
+                    <ArchiveButton
+                      documentId={document.id}
+                      contributorId={doc.email}
+                    />
+                  )}
+                </Card.Text>
+                <Card.Text className="small text-muted d-flex justify-content-between">
+                  To: エリア{areaName(doc.to.area)}
+                  {"　"}
+                  {sectionName(doc.to.section)}
+                  {"　"}
+                  <ConfirmBadge
+                    userInfoEmail={userInfoEmail}
+                    userInfoName={userInfoName}
+                    canAdmin={canAdmin}
+                    documentId={document.id}
+                    contributorId={doc.email}
+                    confirmed={doc.confirmed}
+                  />
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          );
+        }
+      });
+      setContentData(contentArray);
+      setCardData(cardArray);
+    });
+  }, [counter]);
+
+  return (
+    <>
+      <style jsx>{`
+        div {
+          margin-bottom: 4rem;
+        }
+      `}</style>
+
+      <div>
+        <Stack gap={1}>{cardData}</Stack>
+        <ArticleModal
+          show={show}
+          setShow={setShow}
+          title={targetArticle?.title}
+          article={targetArticle?.article}
+        />
+      </div>
+    </>
+  );
+}
+
+export default Archives;
---
+++ circular-for-construction-sites/pages/archives.tsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Header from "../components/Header";
+import ControlPanel from "../components/ControlPanel";
+import SecondLine from "../components/SecondLine";
+import Archives from "../components/Archives";
+import Footer from "../components/Footer";
+import { db, auth } from "../components/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function archives(): JSX.Element {
+  type UserInfo = {
+    admin: boolean;
+    area: string;
+    email: string;
+    name: string;
+    section: string;
+  };
+
+  const [user, loading, error] = useAuthState(auth);
+  const [userInfo, setUserInfo] = useState<UserInfo>({
+    admin: false,
+    area: "",
+    email: "",
+    name: "",
+    section: "",
+  });
+  const [counter, setCounter] = useState(0);
+  const router = useRouter();
+
+  const titleProp: string = "アーカイブ";
+
+  useEffect((): void => {
+    if (user !== null && user !== undefined) {
+      const userId = user.email;
+      if (userId !== null) {
+        getDoc(doc(db, "users", userId)).then((snapshot): void => {
+          const document = snapshot.data() as UserInfo;
+          document && setUserInfo(document);
+        });
+      }
+    }
+  }, []);
+
+  useEffect((): void => {
+    if (user === null) {
+      router.push("/");
+    }
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <p>Initialising User...</p>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (user) {
+    return (
+      <>
+        <Header title={titleProp} />
+        <ControlPanel brand={titleProp} />
+        <SecondLine
+          counter={counter}
+          setCounter={setCounter}
+          name={userInfo.name}
+        />
+        <Archives
+          userInfoEmail={userInfo.email}
+          userInfoName={userInfo.name}
+          canAdmin={userInfo.admin}
+          counter={counter}
+          setCounter={setCounter}
+        />
+        <Footer />
+      </>
+    );
+  }
+  return <></>;
+}
+
+export default archives;
