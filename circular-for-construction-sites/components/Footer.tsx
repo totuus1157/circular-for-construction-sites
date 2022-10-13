893b92fc67f5e68c62e61b7ffecb3da4338beffa
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-10-13 14:44:18 +0900

    アイコンの色を変更

--- circular-for-construction-sites/components/Footer.tsx
+++ circular-for-construction-sites/components/Footer.tsx
@@ -7,21 +7,21 @@ function Footer(): JSX.Element {
     <Navbar className="justify-content-around" fixed="bottom">
       <Link href="/news">
         <a>
-          <Button>
+          <Button variant="dark">
             <i className="bi bi-newspaper"></i>
           </Button>
         </a>
       </Link>
       <Link href="/calendar">
         <a>
-          <Button>
+          <Button variant="dark">
             <i className="bi bi-calendar-date"></i>
           </Button>
         </a>
       </Link>
       <Link href="/archives">
         <a>
-          <Button>
+          <Button variant="dark">
             <i className="bi bi-archive"></i>
           </Button>
         </a>
