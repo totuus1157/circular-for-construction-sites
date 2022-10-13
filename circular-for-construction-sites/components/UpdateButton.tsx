ee21119118bfa6cc9a0a99b61d841faeaa8a805a
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-10-13 14:07:29 +0900

    更新ボタンをアイコンに変更

--- circular-for-construction-sites/components/UpdateButton.tsx
+++ circular-for-construction-sites/components/UpdateButton.tsx
@@ -5,8 +5,12 @@ type Props = { counter: number; setCounter: (arg0: number) => void };
 function UpdateButton(props: Props) {
   const { counter, setCounter } = props;
   return (
-    <Button variant="secondary" size="sm" onClick={(): void => setCounter(counter + 1)}>
-      更新
+    <Button
+      variant="secondary"
+      size="sm"
+      onClick={(): void => setCounter(counter + 1)}
+    >
+      <i className="bi bi-arrow-clockwise" style={{ fontSize: "1.2rem" }}></i>
     </Button>
   );
 }
