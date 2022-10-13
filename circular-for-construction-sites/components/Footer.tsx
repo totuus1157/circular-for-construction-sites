d28090b95c93b219759bb408846f7e2b6839997c
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-10-13 14:30:45 +0900

    回覧板・カレンダー・アーカイブのボタンをアイコンに変更

--- circular-for-construction-sites/components/Footer.tsx
+++ circular-for-construction-sites/components/Footer.tsx
@@ -1,23 +1,29 @@
 import Link from "next/link";
 import Navbar from "react-bootstrap/Navbar";
-import Button from 'react-bootstrap/Button'
+import Button from "react-bootstrap/Button";
 
 function Footer(): JSX.Element {
   return (
     <Navbar className="justify-content-around" fixed="bottom">
       <Link href="/news">
         <a>
-          <Button>回覧板</Button>
+          <Button>
+            <i className="bi bi-newspaper"></i>
+          </Button>
         </a>
       </Link>
       <Link href="/calendar">
         <a>
-          <Button>カレンダー</Button>
+          <Button>
+            <i className="bi bi-calendar-date"></i>
+          </Button>
         </a>
       </Link>
       <Link href="/archives">
         <a>
-          <Button>アーカイブ</Button>
+          <Button>
+            <i className="bi bi-archive"></i>
+          </Button>
         </a>
       </Link>
     </Navbar>
